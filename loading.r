9986d3a7f17a361ca3169589b94729f9c0e197d9
LorthiosTheo <75380734+TheoAlegretti@users.noreply.github.com>
2022-04-05 16:08:00 +0200

    go

---
+++ loading.r
@@ -0,0 +1,14 @@
+#Packages
+
+
+
+
+#Path_s 
+
+#Théo : 
+Path_theo = '/Users/theoalegretti/Documents/GitHub/ENSAE_Time-Series-Project/Malt_Production.csv'
+
+
+df <- read.csv(Path_theo, dec=".",sep=',', header = T)
+
+
